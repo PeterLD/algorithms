3e399b00794c6be0bd3557bf8541e5689ffd3a8d
Peter Lyle-Dugas <p.lyled@gmail.com>
2013-06-22 18:25:07 -0700

    Binary heaps!

--- data_structures/trees.py
+++ data_structures/trees.py
@@ -31,3 +31,111 @@ class BinaryTree:
 
     def get_root_value(self):
         return self.key
+
+
+class MinHeap:
+    def __init__(self):
+        self.heap_list = [0]
+        self.current_size = 0
+
+    def insert(self, item):
+        self.heap_list.append(item)
+        self.current_size = self.current_size + 1
+        self.perc_up(self.current_size)
+
+    def perc_up(self, node_position):
+        while node_position // 2 > 0:
+            if self.heap_list[node_position] < self.heap_list[node_position // 2]:
+                self.heap_list[node_position], self.heap_list[node_position // 2] = self.heap_list[node_position // 2], self.heap_list[node_position]
+            node_position = node_position // 2
+
+    def pop_min(self):
+        min_value = self.heap_list[1]
+
+        self.heap_list[1] = self.heap_list[self.current_size]
+        self.current_size = self.current_size - 1
+        self.heap_list.pop()
+        self.perc_down(1)
+
+        return min_value
+
+    def perc_down(self, node_position):
+        while node_position * 2 <= self.current_size:
+            min_child = self.min_child(node_position)
+
+            if self.heap_list[node_position] > self.heap_list[min_child]:
+                self.heap_list[node_position], self.heap_list[min_child] = self.heap_list[min_child], self.heap_list[node_position]
+
+            node_position = min_child
+
+    def min_child(self, node_position):
+        if node_position * 2 + 1 > self.current_size:
+            return node_position * 2
+        else:
+            if self.heap_list[node_position * 2] < self.heap_list[node_position * 2 + 1]:
+                return node_position * 2
+            else:
+                return node_position * 2 + 1
+
+    def build_heap(self, a_list):
+        i = len(a_list) // 2
+        self.current_size = len(a_list)
+        self.heap_list = [0] + a_list[:]
+
+        while (i > 0):
+            self.perc_down(i)
+            i = i - 1
+
+
+class MaxHeap:
+    def __init__(self):
+        self.heap_list = [0]
+        self.current_size = 0
+
+    def insert(self, item):
+        self.heap_list.append(item)
+        self.current_size = self.current_size + 1
+        self.perc_up(self.current_size)
+
+    def perc_up(self, node_position):
+        while node_position // 2 > 0:
+            if self.heap_list[node_position] > self.heap_list[node_position // 2]:
+                self.heap_list[node_position], self.heap_list[node_position // 2] = self.heap_list[node_position // 2], self.heap_list[node_position]
+            node_position = node_position // 2
+
+    def pop_max(self):
+        max_value = self.heap_list[1]
+
+        self.heap_list[1] = self.heap_list[self.current_size]
+        self.current_size = self.current_size - 1
+        self.heap_list.pop()
+        self.perc_down(1)
+
+        return max_value
+
+    def perc_down(self, node_position):
+        while node_position * 2 <= self.current_size:
+            max_child = self.max_child(node_position)
+
+            if self.heap_list[node_position] < self.heap_list[max_child]:
+                self.heap_list[node_position], self.heap_list[max_child] = self.heap_list[max_child], self.heap_list[node_position]
+
+            node_position = max_child
+
+    def max_child(self, node_position):
+        if node_position * 2 + 1 > self.current_size:
+            return node_position * 2
+        else:
+            if self.heap_list[node_position * 2] > self.heap_list[node_position * 2 + 1]:
+                return node_position * 2
+            else:
+                return node_position * 2 + 1
+
+    def build_heap(self, a_list):
+        i = len(a_list) // 2
+        self.current_size = len(a_list)
+        self.heap_list = [0] + a_list
+
+        while i > 0:
+            self.perc_down(i)
+            i = i - 1
